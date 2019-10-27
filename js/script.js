2eee5d90a0d594b6b0ac772c3429d5fe443ef37a
Julia-kas <julia.kasabutskaya@gmail.com>
2019-10-27 20:59:43 +0100

    Правки

--- js/script.js
+++ js/script.js
@@ -37,3 +37,26 @@ mail_form.addEventListener("submit", function (evt) {
     mail_modal.classList.add("modal-error");
   }
 });
+
+var map_btn = document.querySelector(".map-show");
+var map_modal = document.querySelector(".modal-map");
+var map_close = map_modal.querySelector(".button-close");
+
+map_btn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    map_modal.classList.add("modal-open");
+})
+
+map_close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    map_modal.classList.remove("modal-open");
+})
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (map_modal.classList.contains("modal-open")) {
+            map_modal.classList.remove("modal-open");
+        }
+    }
+});
